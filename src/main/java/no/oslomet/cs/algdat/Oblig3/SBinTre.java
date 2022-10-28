4c2fb55c1f61c8879306332a0cff417d73b4acaf
drissabk <abkhil1999@gmail.com>
2022-10-28 16:54:15 +0200

    svar på oppgave 1

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -3,6 +3,7 @@ package no.oslomet.cs.algdat.Oblig3;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 public class SBinTre<T> {
@@ -82,8 +83,30 @@ public class SBinTre<T> {
         return antall == 0;
     }
 
+    //Oppgave 1
+
     public boolean leggInn(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(verdi, "Null verdier er ikke tillat");
+
+        Node<T> p = rot;
+        Node<T> q = null;
+
+        int cmp = 0;
+
+        while (p != null){
+            q = p;
+            cmp = comp.compare(verdi, p.verdi);
+            p = cmp < 0 ? p.venstre:p.høyre;
+        }
+        p = new Node<>(verdi, q);
+
+        if(q == null) rot = p;
+        else if(cmp < 0) q.venstre =p;
+        else q.høyre = p;
+        antall++;
+        endringer++;
+
+        return true;
     }
 
     public boolean fjern(T verdi) {
